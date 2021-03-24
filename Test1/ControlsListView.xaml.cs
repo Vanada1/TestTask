e2112d7ba761d8dd388e71af67b07368e8a4fb67
Vladimir Shvoev <shvoev.work@gmail.com>
2021-03-24 14:48:08 +0700

    Добавление контролов и базового класса с ошибкой

--- Test1/ControlsListView.xaml.cs
+++ Test1/ControlsListView.xaml.cs
@@ -16,11 +16,11 @@ using System.Windows.Shapes;
 namespace Test1
 {
     /// <summary>
-    /// Interaction logic for UserControl1.xaml
+    /// Interaction logic for ControlsListView.xaml
     /// </summary>
-    public partial class UserControl1 : UserControl
+    public partial class ControlsListView : UserControl
     {
-        public UserControl1()
+        public ControlsListView()
         {
             InitializeComponent();
         }
